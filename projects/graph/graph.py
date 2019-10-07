9ee75223275999cd75cf7eeffaaab7b6ce543006
Lidii <lidiiany92@gmail.com>
2019-10-07 14:46:18 -0700

    dfs

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -92,14 +92,62 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        
+
+        visited = []
+        queue = Queue()
+        queue.enqueue([starting_vertex])
+
+
+        while queue.size() > 0:
+            shortest_path = queue.dequeue()
+            vertex = shortest_path[-1]
+
+            if vertex not in visited:
+
+                if vertex is destination_vertex:
+                    print("BFS")
+                    return shortest_path
+
+                visited.append(vertex)
+
+                for neighbor in self.vertices[vertex]:
+                    new_shortest_path = list(shortest_path)
+                    new_shortest_path.append(neighbor)
+                    queue.enqueue(new_shortest_path)
+        
+        return None
+
+
     def dfs(self, starting_vertex, destination_vertex):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
         depth-first order.
         """
-        pass  # TODO
+        visited = []
+        stack = Stack()
+        stack.push([starting_vertex])
+
+
+        while stack.size() > 0:
+            shortest_path = stack.pop()
+            vertex = shortest_path[-1]
+
+            if vertex not in visited:
+
+                if vertex is destination_vertex:
+                    print("DFS")
+                    return shortest_path
+
+                visited.append(vertex)
+
+                for neighbor in self.vertices[vertex]:
+                    new_shortest_path = list(shortest_path)
+                    new_shortest_path.append(neighbor)
+                    stack.push(new_shortest_path)
+        
+        return None
 
 
 
