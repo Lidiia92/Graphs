c181148c2a45bf3b3f6ffe42b1118540b60c1bad
Lidii <lidiiany92@gmail.com>
2019-10-08 13:08:02 -0700

    Set up graph and queue

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,48 @@
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+
+class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+
+
+    def add_vertex(self, vertex):
+        """
+        Add a vertex to the graph.
+        """
+        if vertex not in self.vertices:
+            self.vertices[vertex] = set()
+        else:
+            print("VERTEX ALREADY EXISTS")
+
+
+
+    def add_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph.
+        """
+        if v1 not in self.vertices or v2 not in self.vertices:
+            print("VERTEX DOES NOT EXIST")
+        else:
+            #Setting direction from v1 to v2
+            self.vertices[v1].add(v2)
+            # self.vertices[v2].add(v1)
+
+
+
+
 def earliest_ancestor(ancestors, starting_node):
     pass
